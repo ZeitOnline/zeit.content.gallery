e520ed8b101ee3730003640d94364f778899be67
Christian Zagrodnick <cz@gocept.com>
2009-05-28 11:49:55 +0000

    1.22

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.gallery',
-    version='1.22dev',
+    version='1.22',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
